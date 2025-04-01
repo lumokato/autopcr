56f25701d380d1a5ca418b39ef0a9c9992d73b71
lumokato <suzto@qq.com>
2025-04-01 14:02:36 +0800

    default

--- autopcr/module/modules/hatsune.py
+++ autopcr/module/modules/hatsune.py
@@ -56,7 +56,7 @@ class hatsune_h_sweep(Module):
         if is_skip: raise SkipError("")
 
 @name('扫荡活动vh本boss')
-@default(True)
+@default(False)
 class hatsune_vhboss_sweep(Module):
     async def do_task(self, client: pcrclient):
         is_error = False
